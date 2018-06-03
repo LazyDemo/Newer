ebac222a7d4b7f1992c4564f59599fb034386a3c
Majing <810208873@qq.com>
2018-06-03 20:41:03 +0800

    create mainapplication, splashActivity

---
+++ app/src/main/java/com/wb/newer/newer/MainApplication.java
@@ -0,0 +1,15 @@
+package com.wb.newer.newer;
+
+import android.app.Application;
+
+/**
+ * Created by lazydemo on 2018/06/03 0003.
+ * email: lazydemo@163.com
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
