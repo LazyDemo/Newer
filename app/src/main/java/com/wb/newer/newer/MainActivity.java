ea7988d7fea9545c1ef478b2a9d3431ba817453c
吴博 <bo.wu@jian24.com>
2018-06-05 15:02:19 +0800

    change

--- app/src/main/java/com/wb/newer/newer/MainActivity.java
+++ app/src/main/java/com/wb/newer/newer/MainActivity.java
@@ -35,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }
