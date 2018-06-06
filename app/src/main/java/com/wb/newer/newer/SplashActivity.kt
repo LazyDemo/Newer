20787ece4231f9eecc709eecf32b91c4c726b149
吴博 <bo.wu@jian24.com>
2018-06-06 17:42:21 +0800

    constraintlayout anim1

--- app/src/main/java/com/wb/newer/newer/SplashActivity.kt
+++ app/src/main/java/com/wb/newer/newer/SplashActivity.kt
@@ -5,12 +5,18 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.constraint.ConstraintSet
+import android.support.transition.ChangeBounds
+import android.support.transition.TransitionManager
 import android.util.Log
 import android.view.Window
+import android.view.animation.AnticipateOvershootInterpolator
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     var temp = -1
+
+    private lateinit var constraintSet: ConstraintSet
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +32,7 @@ class SplashActivity : AppCompatActivity() {
                 if (temp == charArray.size - 1) {
                     lottieFontViewGroup.endListener = object : LottieFontViewGroup.EndListener {
                         override fun onEnd() {
-                            startMain()
+//                            startMain()
                         }
 
                     }
@@ -37,6 +43,22 @@ class SplashActivity : AppCompatActivity() {
 
         }
         animator.start()
+
+        constraintSet = ConstraintSet()
+        constraintSet.clone(this, R.layout.activity_splash_second)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler().postDelayed({
+            val transition = ChangeBounds()
+            transition.interpolator = AnticipateOvershootInterpolator(1.0f)
+            transition.duration = 2000
+            TransitionManager.beginDelayedTransition(constraintLayout, transition)
+            constraintSet.applyTo(constraintLayout)
+        }, 2000)
     }
 
     override fun onPause() {
