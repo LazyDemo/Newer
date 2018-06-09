5d26103da86957462da6857bc18ac4293976e61c
lazydemo <lazydemo@163.com>
2018-06-09 17:16:12 +0800

    statusbar reset

--- app/src/main/java/com/wb/newer/newer/MainActivity.kt
+++ app/src/main/java/com/wb/newer/newer/MainActivity.kt
@@ -11,6 +11,15 @@ import android.widget.Toast
 import com.wb.newer.newer.home.IndexFragment
 import com.wb.newer.newer.home.SecondFragment
 import com.wb.newer.newer.home.ThirdFragment
+import android.view.WindowManager
+import android.os.Build
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+
 
 class MainActivity : AppCompatActivity(),IndexFragment.OnFragmentInteractionListener,SecondFragment.OnFragmentInteractionListener,ThirdFragment.OnFragmentInteractionListener {
     override fun onIndexEvent(uri: Uri) {
@@ -55,7 +64,7 @@ class MainActivity : AppCompatActivity(),IndexFragment.OnFragmentInteractionList
         val navigation = findViewById<BottomNavigationView>(R.id.navigation)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         savedInstanceState ?: showFragment(indexFragment)
-
+        setTransparentStatusBar(this)
     }
 
     private fun showFragment(fragment: Fragment) {
@@ -64,4 +73,21 @@ class MainActivity : AppCompatActivity(),IndexFragment.OnFragmentInteractionList
                 .commit()
     }
 
+
+    fun setTransparentStatusBar(activity: Activity) {
+        //5.0及以上
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.navigationBarColor = Color.TRANSPARENT
+            //4.4到5.0
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val localLayoutParams = activity.window.attributes
+            localLayoutParams.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or localLayoutParams.flags
+        }
+    }
 }
