2664539e3e251a3fb2b30843f8662daebc6f7d1d
吴博 <bo.wu@jian24.com>
2018-06-12 18:49:04 +0800

    test

--- app/src/main/java/com/wb/newer/newer/home/SecondFragment.kt
+++ app/src/main/java/com/wb/newer/newer/home/SecondFragment.kt
@@ -1,13 +1,17 @@
 package com.wb.newer.newer.home
 
 import android.arch.lifecycle.ViewModelProviders
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
 
 import com.wb.newer.newer.R
+import kotlinx.android.synthetic.main.second_fragment.*
 
 class SecondFragment : Fragment() {
 
@@ -26,6 +30,7 @@ class SecondFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SecondViewModel::class.java)
         // TODO: Use the ViewModel
+
     }
 
 }
