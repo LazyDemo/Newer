f9cacc0a44fc2a056338e0eb25d318496323e8cd
吴博 <bo.wu@jian24.com>
2018-06-15 17:21:42 +0800

    svg support

--- app/src/main/java/com/wb/newer/newer/MainApplication.java
+++ app/src/main/java/com/wb/newer/newer/MainApplication.java
@@ -2,12 +2,17 @@ package com.wb.newer.newer;
 
 import android.app.Application;
 import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AppCompatDelegate;
 
 /**
  * Created by lazydemo on 2018/06/03 0003.
  * email: lazydemo@163.com
  */
 public class MainApplication extends MultiDexApplication {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     public void onCreate() {
