232993288246c514c4bbafa639e6a490d7fbc6e1
吴博 <bo.wu@jian24.com>
2018-06-26 15:09:18 +0800

    增加文章列表model

---
+++ app/src/main/java/com/wb/newer/newer/model/data/HomeResponse.java
@@ -0,0 +1,322 @@
+package com.wb.newer.newer.model.data;
+
+import java.util.List;
+
+/**
+ * company: guoxuekeji
+ * product: jian24
+ * Created by wb on 2018/6/26.
+ * Description:
+ */
+public class HomeResponse {
+
+    /**
+     * curPage : 2
+     * datas : [{"apkLink":"","author":"尘封的落叶","chapterId":262,"chapterName":"SDK开发","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3039,"link":"https://www.jianshu.com/p/4d092c915ef1","niceDate":"2天前","origin":"","projectLink":"","publishTime":1529741359000,"superChapterId":191,"superChapterName":"热门专题","tags":[],"title":"AndroidStudio封装SDK的那些事","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"wisedeveloper","chapterId":294,"chapterName":"完整项目","collect":false,"courseId":13,"desc":"练手小项目,WanAndroid客户端Java版, 技术架构:MVP + okhttp-OkGo + RxJava2 + Glide","envelopePic":"http://www.wanandroid.com/blogimgs/6943d423-e68e-482e-b35d-5dc777bc3afa.png","fresh":false,"id":3037,"link":"http://www.wanandroid.com/blog/show/2166","niceDate":"2018-06-22","origin":"","projectLink":"https://github.com/wisedeveloper/WanAndroid","publishTime":1529657049000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=294"}],"title":"WanAndroid客户端Java版","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"jenly1314","chapterId":323,"chapterName":"动画","collect":false,"courseId":13,"desc":"GiftSurfaceView 最初出自于2014年开发HalloStar项目时所写，主要用于HalloStar项目直播间的送礼物动画。现在想来，那夕阳下的奔跑，是我逝去的青春。","envelopePic":"http://www.wanandroid.com/blogimgs/63360c5d-52a6-47a4-b51b-dde95616ecc8.png","fresh":false,"id":3036,"link":"http://www.wanandroid.com/blog/show/2165","niceDate":"2018-06-22","origin":"","projectLink":"https://github.com/jenly1314/GiftSurfaceView","publishTime":1529656806000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=323"}],"title":"GiftSurfaceView 直播间送礼物动画","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"Loren1994","chapterId":358,"chapterName":"项目基础功能","collect":false,"courseId":13,"desc":"支持到android8.0,简单小巧.希望对你有帮助.","envelopePic":"http://www.wanandroid.com/resources/image/pc/default_project_img.jpg","fresh":false,"id":3034,"link":"http://www.wanandroid.com/blog/show/2163","niceDate":"2018-06-22","origin":"","projectLink":"https://github.com/Loren1994/AndroidUpdate","publishTime":1529656524000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=358"}],"title":"android小巧的升级库","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"Loren1994","chapterId":314,"chapterName":"RV列表动效","collect":false,"courseId":13,"desc":"这是封装的带分类Recyclerview的Adapter，主要解决项目中分类列表繁琐的重复劳动。  ","envelopePic":"http://www.wanandroid.com/blogimgs/879cffaa-97d6-456b-bf9b-b7e6a176cf0e.png","fresh":false,"id":3033,"link":"http://www.wanandroid.com/blog/show/2162","niceDate":"2018-06-22","origin":"","projectLink":"https://github.com/Loren1994/UniversalTypeRecyclerView","publishTime":1529656448000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=314"}],"title":"封装带分类的RecyclerViewAdapter UniversalTypeRecyclerView","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"拉丁吴","chapterId":261,"chapterName":"屏幕适配","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3032,"link":"https://www.jianshu.com/p/a4b8e4c5d9b0","niceDate":"2018-06-22","origin":"","projectLink":"","publishTime":1529653793000,"superChapterId":135,"superChapterName":"项目必备","tags":[],"title":"Android 稳定和高效的UI适配方案","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"prototypez","chapterId":198,"chapterName":"基础概念","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3031,"link":"http://prototypez.github.io/2018/06/06/using-save-state/","niceDate":"2018-06-22","origin":"","projectLink":"","publishTime":1529653215000,"superChapterId":168,"superChapterName":"基础知识","tags":[],"title":"不需要再手写 onSaveInstanceState 了，因为你的时间非常值钱","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"JerryloveEmily","chapterId":202,"chapterName":"机器学习&人工智能","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3030,"link":"https://www.jianshu.com/p/d8811e565bd2%20","niceDate":"2018-06-21","origin":"","projectLink":"","publishTime":1529545885000,"superChapterId":202,"superChapterName":"延伸技术","tags":[],"title":"人工智能数学基础----矩阵","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"HuangTao_Zoey ","chapterId":60,"chapterName":"Android Studio相关","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3029,"link":"https://www.jianshu.com/p/f406d535a8bc","niceDate":"2018-06-20","origin":"","projectLink":"","publishTime":1529466669000,"superChapterId":60,"superChapterName":"开发环境","tags":[],"title":"Android trace文件抓取原理","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"hexianqiao3755","chapterId":358,"chapterName":"项目基础功能","collect":false,"courseId":13,"desc":"仿京东、天猫app的商品详情页的布局架构, 以及功能实现","envelopePic":"http://www.wanandroid.com/blogimgs/4cc74c21-e3c1-466e-9902-ba7bfd7e1eb1.png","fresh":false,"id":3028,"link":"http://www.wanandroid.com/blog/show/2161","niceDate":"2018-06-19","origin":"","projectLink":"https://github.com/hexianqiao3755/GoodsInfoPage","publishTime":1529408855000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=358"}],"title":"仿京东、天猫app的商品详情页的布局架构 GoodsInfoPage","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"git-xuhao","chapterId":294,"chapterName":"完整项目","collect":false,"courseId":13,"desc":"基于Kotlin+MVP+Retrofit+RxJava+Glide 等架构实现短视频类APP练手项目，UI简约，详细注释。","envelopePic":"http://www.wanandroid.com/blogimgs/4e98e9c8-846a-416a-ad6e-5f867f5f5b6f.png","fresh":false,"id":3025,"link":"http://www.wanandroid.com/blog/show/2158","niceDate":"2018-06-19","origin":"","projectLink":"https://github.com/git-xuhao/KotlinMvp","publishTime":1529408178000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=294"}],"title":"KotlinMVP实现短视频类APP","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"骑小猪看流星","chapterId":262,"chapterName":"SDK开发","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3024,"link":"https://www.jianshu.com/p/8b9d82560a67","niceDate":"2018-06-19","origin":"","projectLink":"","publishTime":1529391687000,"superChapterId":191,"superChapterName":"热门专题","tags":[],"title":"Android兼容H5游戏SDK开发详解","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"大大纸飞机","chapterId":245,"chapterName":"集合相关","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3023,"link":"https://www.jianshu.com/p/407afb4a267a  ","niceDate":"2018-06-19","origin":"","projectLink":"","publishTime":1529372331000,"superChapterId":245,"superChapterName":"Java深入","tags":[],"title":"荐一个面试必问的Java系列专题","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"anhao_chan","chapterId":228,"chapterName":"辅助 or 工具类","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3022,"link":"https://www.jianshu.com/p/9d6e6263c93b","niceDate":"2018-06-18","origin":"","projectLink":"","publishTime":1529304799000,"superChapterId":135,"superChapterName":"项目必备","tags":[],"title":"那些有意思的Android开发工具系列三（stetho）","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"BrightVan","chapterId":169,"chapterName":"gradle","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3021,"link":"https://www.jianshu.com/p/341744b98aaf","niceDate":"2018-06-18","origin":"","projectLink":"","publishTime":1529303950000,"superChapterId":60,"superChapterName":"开发环境","tags":[],"title":"Android gradle provided、implementation等指令注意点","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"滑板上的老砒霜","chapterId":97,"chapterName":"音视频","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3020,"link":"https://juejin.im/post/5b1e2a04f265da6e4f7a112b","niceDate":"2018-06-18","origin":"","projectLink":"","publishTime":1529302647000,"superChapterId":95,"superChapterName":"多媒体技术","tags":[],"title":"Android录音并输出为Mp4文件","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"tome869535144","chapterId":185,"chapterName":"组件化","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3019,"link":"https://www.jianshu.com/p/ba32488f9555","niceDate":"2018-06-18","origin":"","projectLink":"","publishTime":1529301107000,"superChapterId":191,"superChapterName":"热门专题","tags":[],"title":"从零开始搭建Android组件化框架","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"canhuah","chapterId":371,"chapterName":"Flutter项目","collect":false,"courseId":13,"desc":"仿WanAndroid客户端 Flutter版 ~","envelopePic":"http://www.wanandroid.com/blogimgs/e0b09cc9-41c6-4144-8439-43f505dbf6e2.png","fresh":false,"id":3018,"link":"http://www.wanandroid.com/blog/show/2157","niceDate":"2018-06-18","origin":"","projectLink":"https://github.com/canhuah/WanAndroid","publishTime":1529292178000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=371"}],"title":"仿WanAndroid客户端 Flutter版 ","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"58架构师","chapterId":270,"chapterName":"埋点","collect":false,"courseId":13,"desc":"","envelopePic":"","fresh":false,"id":3017,"link":"https://mp.weixin.qq.com/s?__biz=MzI1NDc5MzIxMw==&mid=2247483814&idx=1&sn=2e19a842de5373a5e13864fbbed77d04","niceDate":"2018-06-17","origin":"","projectLink":"","publishTime":1529244293000,"superChapterId":135,"superChapterName":"项目必备","tags":[],"title":"58无埋点数据采集技术在Android端实践","type":0,"userId":-1,"visible":1,"zan":0},{"apkLink":"","author":"xiaweizi","chapterId":370,"chapterName":"微信小程序","collect":false,"courseId":13,"desc":"小程序 的火热程度我就不多说了，我之前对这个就蛮有兴趣的，于是花了大概5天的时间，完成了 学习-入门-写项目 这一套流程。","envelopePic":"http://www.wanandroid.com/blogimgs/b2f3ade5-fe52-4a73-a6b6-1f819c18a332.png","fresh":false,"id":3016,"link":"http://www.wanandroid.com/blog/show/2156","niceDate":"2018-06-15","origin":"","projectLink":"https://github.com/xiaweizi/QNews_we_chat","publishTime":1529074538000,"superChapterId":294,"superChapterName":"开源项目主Tab","tags":[{"name":"项目","url":"/project/list/1?cid=370"}],"title":"开源小程序 - 趣闻 QNews_we_chat","type":0,"userId":-1,"visible":1,"zan":0}]
+     * offset : 20
+     * over : false
+     * pageCount : 70
+     * size : 20
+     * total : 1394
+     */
+
+    private int curPage;
+    private int offset;
+    private boolean over;
+    private int pageCount;
+    private int size;
+    private int total;
+    private List<DatasBean> datas;
+
+    public int getCurPage() {
+        return curPage;
+    }
+
+    public void setCurPage(int curPage) {
+        this.curPage = curPage;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public List<DatasBean> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<DatasBean> datas) {
+        this.datas = datas;
+    }
+
+    public static class DatasBean {
+        /**
+         * apkLink :
+         * author : 尘封的落叶
+         * chapterId : 262
+         * chapterName : SDK开发
+         * collect : false
+         * courseId : 13
+         * desc :
+         * envelopePic :
+         * fresh : false
+         * id : 3039
+         * link : https://www.jianshu.com/p/4d092c915ef1
+         * niceDate : 2天前
+         * origin :
+         * projectLink :
+         * publishTime : 1529741359000
+         * superChapterId : 191
+         * superChapterName : 热门专题
+         * tags : []
+         * title : AndroidStudio封装SDK的那些事
+         * type : 0
+         * userId : -1
+         * visible : 1
+         * zan : 0
+         */
+
+        private String apkLink;
+        private String author;
+        private int chapterId;
+        private String chapterName;
+        private boolean collect;
+        private int courseId;
+        private String desc;
+        private String envelopePic;
+        private boolean fresh;
+        private int id;
+        private String link;
+        private String niceDate;
+        private String origin;
+        private String projectLink;
+        private long publishTime;
+        private int superChapterId;
+        private String superChapterName;
+        private String title;
+        private int type;
+        private int userId;
+        private int visible;
+        private int zan;
+        private List<?> tags;
+
+        public String getApkLink() {
+            return apkLink;
+        }
+
+        public void setApkLink(String apkLink) {
+            this.apkLink = apkLink;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public int getChapterId() {
+            return chapterId;
+        }
+
+        public void setChapterId(int chapterId) {
+            this.chapterId = chapterId;
+        }
+
+        public String getChapterName() {
+            return chapterName;
+        }
+
+        public void setChapterName(String chapterName) {
+            this.chapterName = chapterName;
+        }
+
+        public boolean isCollect() {
+            return collect;
+        }
+
+        public void setCollect(boolean collect) {
+            this.collect = collect;
+        }
+
+        public int getCourseId() {
+            return courseId;
+        }
+
+        public void setCourseId(int courseId) {
+            this.courseId = courseId;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public String getEnvelopePic() {
+            return envelopePic;
+        }
+
+        public void setEnvelopePic(String envelopePic) {
+            this.envelopePic = envelopePic;
+        }
+
+        public boolean isFresh() {
+            return fresh;
+        }
+
+        public void setFresh(boolean fresh) {
+            this.fresh = fresh;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+        public String getNiceDate() {
+            return niceDate;
+        }
+
+        public void setNiceDate(String niceDate) {
+            this.niceDate = niceDate;
+        }
+
+        public String getOrigin() {
+            return origin;
+        }
+
+        public void setOrigin(String origin) {
+            this.origin = origin;
+        }
+
+        public String getProjectLink() {
+            return projectLink;
+        }
+
+        public void setProjectLink(String projectLink) {
+            this.projectLink = projectLink;
+        }
+
+        public long getPublishTime() {
+            return publishTime;
+        }
+
+        public void setPublishTime(long publishTime) {
+            this.publishTime = publishTime;
+        }
+
+        public int getSuperChapterId() {
+            return superChapterId;
+        }
+
+        public void setSuperChapterId(int superChapterId) {
+            this.superChapterId = superChapterId;
+        }
+
+        public String getSuperChapterName() {
+            return superChapterName;
+        }
+
+        public void setSuperChapterName(String superChapterName) {
+            this.superChapterName = superChapterName;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public void setUserId(int userId) {
+            this.userId = userId;
+        }
+
+        public int getVisible() {
+            return visible;
+        }
+
+        public void setVisible(int visible) {
+            this.visible = visible;
+        }
+
+        public int getZan() {
+            return zan;
+        }
+
+        public void setZan(int zan) {
+            this.zan = zan;
+        }
+
+        public List<?> getTags() {
+            return tags;
+        }
+
+        public void setTags(List<?> tags) {
+            this.tags = tags;
+        }
+    }
+}
